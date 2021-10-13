1dd4b77b9a472b0017f485b8f58c192ac9c9d4be
William Grant <hello@wpg.io>
2021-10-12 17:42:12 -0700

    react box

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,5 +1,7 @@
 const Raffle = artifacts.require("Raffle");
+const SimpleStorage = artifacts.require("SimpleStorage");
 
 module.exports = function (deployer) {
   deployer.deploy(Raffle);
+  deployer.deploy(SimpleStorage);
 };
