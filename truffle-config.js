a5cbd092d32ee1d51f16d7caa8b25d8e0065ee41
William Grant <hello@wpg.io>
2021-11-16 21:38:16 -0800

    rinkeby deploy code

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,11 @@
 const path = require("path");
 
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const dotenv = require('dotenv');
+dotenv.config();
+const private_key = process.env.ETH_RINKEBY_PRIVATE_KEY;
+const infura_url = process.env.INFURA_RINKEBY_URL;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -7,6 +13,11 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider({privateKeys: [private_key], providerOrUrl: infura_url}),
+      network_id: "4",
+      gas: 5500000
     }
   }
 };
