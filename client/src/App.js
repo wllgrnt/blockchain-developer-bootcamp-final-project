f7d691d854e74f5b46282d82dd4b1d661b5450f3
William Grant <hello@wpg.io>
2021-11-21 18:06:44 -0800

    working state management

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,11 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
-// import getWeb3 from "./getWeb3";
 import Web3 from "web3";
 
 import "./App.css";
-// import getWeb3 from "./getWeb3";
-// import getWeb3 from "./getWeb3";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
-
-  // componentDidMount = async () => {
-  //   try {
-  // //     // Get network provider and web3 instance.
-  //     // const web3 = await getWeb3();
-  //     const web3 = new Web3(window.ethereum);
-  //     this.setState({ web3});
-  //   }
-  //   catch (error) {
-  //     alert('Failed to load web3');
-  //     console.error(error);
-  //   }
-  //   };
-  //     // Use web3 to get the user's accounts.
-  //     // const accounts = await web3.eth.getAccounts();
-
-  //     // Get the contract instance.
-  //     // const networkId = await web3.eth.net.getId();
-  //     // const deployedNetwork = SimpleStorageContract.networks[networkId];
-  //     // const instance = new web3.eth.Contract(
-  //     //   SimpleStorageContract.abi,
-  //     //   deployedNetwork && deployedNetwork.address,
-  //     // );
-
-  //     // Set web3, accounts, and contract to the state, and then proceed with an
-  //     // example of interacting with the contract's methods.
-  //   //   this.setState({ web3, accounts, contract: instance }, this.runExample);
-  //   } catch (error) {
-  //     // Catch any errors for any of the above operations.
-  //     alert(
-  //       `Failed to load web3, accounts, or contract. Check console for details.`,
-  //     );
-  //     console.error(error);
-  //   }
-  // };
-
-  // runExample = async () => {
-  //   const { accounts, contract } = this.state;
-
-  //   // Stores a given value, 5 by default.
-  //   await contract.methods.set(5).send({ from: accounts[0] });
-
-  //   // Get the value from the contract to prove it worked.
-  //   const response = await contract.methods.get().call();
-
-  //   // Update state with the result.
-  //   this.setState({ storageValue: response });
-  // };
+  state = { storageValue: 0, web3: null, accounts: null, contract: null, address: null };
 
   getWeb3 = async () => {
       if (window.ethereum) {
@@ -89,7 +38,9 @@ class App extends Component {
         SimpleStorageContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
-      this.setState({ web3, accounts, contract: instance });
+      console.log(instance.methods);
+      const value = await instance.methods.get().call();
+      this.setState({ web3, accounts, contract: instance, address: deployedNetwork.address, storageValue: value});
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -99,26 +50,44 @@ class App extends Component {
     }
   };
 
-  render() {
-    // if (!this.state.web3) {
-    //   return <div>Loading Web3, accounts, and contract...</div>;
-    // }
+  setStorageValueToFive = async (event) => {
+    try {
+    await this.state.contract.methods.set(5).send({from: this.state.accounts[0]});
+    const value = await this.state.contract.methods.get().call();
+    this.setState({storageValue: value});
+    }
+    catch (error) {
+      alert('transaction failed');
+      console.error(error);
+    }
+  };
 
+  setStorageValueToZero = async (event) => {
+    try {
+    await this.state.contract.methods.set(0).send({from: this.state.accounts[0]});
+    const value = await this.state.contract.methods.get().call();
+    this.setState({storageValue: value});
+    }
+    catch (error) {
+      alert('transaction failed');
+      console.error(error);
+    }
+  };
+
+
+  render() {
     return (
       <div className="App">
         <button onClick={this.connectToWallet.bind(this)}>Connect</button>
-
         <h1>Good to Go!</h1>
         <p>Your Truffle Box is installed and ready.</p>
         <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <p>Connected with account: {this.state.accounts}</p>
+        <p>Contract address: {this.state.address} </p>
+        <p>Contract stored value is: {this.state.storageValue}</p>
+        <button onClick={this.setStorageValueToFive.bind(this)}>Set storage to 5</button>
+        <button onClick={this.setStorageValueToZero.bind(this)}>Set storage to 0</button>
+
       </div>
     );
   }
