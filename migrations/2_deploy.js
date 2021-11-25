8441f07211477eeb2848cd9eca37069668ca9822
William Grant <hello@wpg.io>
2021-11-24 16:50:56 -0800

    purge simplestorage example

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,7 +1,5 @@
 const Raffle = artifacts.require("Raffle");
-const SimpleStorage = artifacts.require("SimpleStorage");
 
 module.exports = function (deployer) {
   deployer.deploy(Raffle);
-  deployer.deploy(SimpleStorage);
 };
